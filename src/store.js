c05e4e75edf79ebcd5d4fc623c3b86cd437035af
sandsunset <sejoon0606@gmail.com>
2025-05-11 05:17:18 +0900

    ✨  유저 전역 state

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import messengerReducer from "./features/messenger/messengerSlice";
 import remoteReducer from "./features/remote/remoteSlice";
+import userReducer from "./features/user/userSlice";
 
 export default configureStore({
   reducer: {
     messenger: messengerReducer,
     remote: remoteReducer,
+    user: userReducer,
   },
 });
