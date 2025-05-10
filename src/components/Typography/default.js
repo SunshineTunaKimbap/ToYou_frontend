9010b8777dc566debdf4be02e5db3d8c58843ffe
sandsunset <sejoon0606@gmail.com>
2025-05-11 02:14:51 +0900

    💄  외부 폰트 추가

--- src/components/Typography/default.js
+++ src/components/Typography/default.js
@@ -1,5 +1,16 @@
+import DungGeunMo from "../../assets/fonts/DungGeunMo.ttf";
+
 import styled from "styled-components";
 
 export const Text = styled.p`
+  @font-face {
+    font-family: 'DugGeunMo';
+    src: url(${DungGeunMo}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+
+    color: black;
+  }
+
   letter-spacing: -0.5px;
 `;
